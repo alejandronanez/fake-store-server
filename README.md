86da1c7ce587759e2010210cd2cced744b65d917
Alejandro Nanez <alejonanez@gmail.com>
2016-01-29 23:23:47 -0500

    - npm commands - Version bump - Updated README.md

---
+++ README.md
@@ -0,0 +1,27 @@
+# Fake Store Server
+
+Fake server for a department store. It generates random data for products and for one store.
+
+Usage
+===
+
+* `npm start` / `npm run server` Starts the server with no data on `localhost:4000`
+
+* `npm run server-populated [X]` Generates `X` products and starts the server on `localhost:4000`
+
+Endpoints
+===
+
+* `/posts` Get all posts
+* `/posts/:post-id` Get an specific post
+* `/stores` Get the unique store in the DB
+* `/cart` Get all items from the cart
+
+Contribute
+===
+
+Feel free to contribute, PRs welcome.
+
+License
+===
+MIT - [Alejandro Ñáñez Ortiz](http://github.com/alejandronanez)
--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+#!/usr/bin/env node
 'use strict';
+const version = require('./package.json').version;
 const products = require('./generators/products');
 const stores = require('./generators/stores');
+const program = require('commander');
+
+program.version(version)
+        .option('--products [number]', 'Generate [number] products', 0)
+        .parse(process.argv);
+
+let data = {
+    products: [],
+    stores: stores.generate(),
+    cart: []
+};
+
+if (program.products) {
+    data.products = products.generate(Number.parseInt(program.products));
+}
 
 module.exports = function () {
-    return {
-        products: products.generate(20),
-        stores: stores.generate(),
-        carts: []
-    };
+    return data;
 };
